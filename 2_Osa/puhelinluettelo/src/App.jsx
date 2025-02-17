a5fa486dd790c719bf09c39a57fdcf53df03f8ab
Fapacus <142172371+Fapacus@users.noreply.github.com>
2025-02-18 01:33:42 +0200

    2 osa 2.15*

--- 2_Osa/puhelinluettelo/src/App.jsx
+++ 2_Osa/puhelinluettelo/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const Filter = ({newFilter, handleFilterChange}) =>
   <div>
     filter shown with <input value={newFilter} onChange={handleFilterChange} />
   </div>
 
-const PersonForm = ({addName, newName, handleNameChange, newNumber, handleNumberChange}) =>
-  <form onSubmit={addName}>
+const PersonForm = ({addPerson, newName, handleNameChange, newNumber, handleNumberChange}) =>
+  <form onSubmit={addPerson}>
     <div>
       name: <input value={newName} onChange={handleNameChange} /> 
     </div>
@@ -19,12 +19,13 @@ const PersonForm = ({addName, newName, handleNameChange, newNumber, handleNumber
     </div>
   </form>
 
-
-
-const Persons = ({personsToShow}) => 
+const Persons = ({ personsToShow, deletePerson}) => 
   <div>
     {personsToShow.map(person =>
-      <p key={person.name}>{person.name} {person.number}</p>
+      <p key={person.id}>
+        {person.name} {person.number}
+        <button onClick={() => deletePerson(person.id, person.name)}>delete</button>
+      </p>
     )}
   </div>
 
@@ -36,14 +37,54 @@ const App = () => {
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
+    personService
+      .getAll()
+      .then((initialPersons) => {
         console.log('promise fulfilled')
-        setPersons(response.data)
+        setPersons(initialPersons)
       })
   }, [])
   console.log('render', persons.length, 'persons')
+
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+
+    const personObject = {
+      name: newName, 
+      number: newNumber 
+    }
+
+    const oldPerson = (persons.find(person => person.name === newName))
+    if (oldPerson) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        personService
+          .update(oldPerson.id, personObject)
+          .then((returnedPerson) => {
+            setPersons(persons.map(person => person.id !== oldPerson.id ? person : returnedPerson))
+          })
+      }
+    }
+    
+    personService
+      .create(personObject)
+      .then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
+  }
+
+  const deletePerson = (id, name) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      personService
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+        })
+    }
+  }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -59,30 +100,10 @@ const App = () => {
     setNewFilter(event.target.value)
   }
 
-  const addName = (event) => {
-    event.preventDefault()
-    console.log('button clicked', event.target)
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
-      return
-    }
-
-    const nameObject = {
-      name: newName, 
-      number: newNumber 
-    }
-    axios
-      .post('http://localhost:3001/persons', nameObject)
-      .then(response => {
-        setPersons(persons.concat(response.data))
-        setNewName('')
-        setNewNumber('')
-      })
-  }
 
   const personsToShow = newFilter === ''
-  ? persons
-  : persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
 
   return (
     <div>
@@ -90,10 +111,10 @@ const App = () => {
       <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
 
       <h2>add a new</h2>
-      <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
 
       <h2>Numbers</h2>
-      <Persons personsToShow={personsToShow} />
+      <Persons personsToShow={personsToShow} deletePerson={deletePerson} />
     </div>
   )
 
