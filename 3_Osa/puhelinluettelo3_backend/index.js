59be80ae3462af52b7ca941e686c06c0a984ecc1
Fapacus <142172371+Fapacus@users.noreply.github.com>
2025-04-18 13:16:08 +0300

    uibuild

--- 3_Osa/puhelinluettelo3_backend/index.js
+++ 3_Osa/puhelinluettelo3_backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+// const cors = require('cors')
 
 const app = express()
 
@@ -10,7 +10,7 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('dist'))
-app.use(cors())
+// app.use(cors())
 
 let persons = [
     {
