bb0a5905679e9b4ec9a2ae1cc97db972f8ea6ca6
Fapacus <142172371+Fapacus@users.noreply.github.com>
2025-04-19 13:08:36 +0300

    general root-push

---
+++ 3_Osa/puhelinluettelo3_backend/models/person.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+if (!url) {
+  console.error('Error: MONGODB_URI missing :(')
+  process.exit(1)
+}
+
+console.log('connecting to MongoDB . . .') 
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('error occured while connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
